de96f36e8e8b4e39c1117e39b3432931f43b8c21
r1ghtwr0ng <rightwrong@protonmail.com>
2022-02-28 22:40:25 +0000

    Added routes.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,40 +1,14 @@
-import { greet } from "./common/greet";
 import fastify from 'fastify';
-import { Message } from "./common/message";
-import { pipe } from "fp-ts/lib/function";
-import { fold } from "fp-ts/lib/Either";
-
-console.log(greet('World'));
-
-// Ad-hoc database
-const msgs = new Array<Message>()
 
 const app = fastify({
     logger: true
 })
 
-// Reply using return value
-app.post('/send', async (req, res) => {
-    return pipe( // Allows to write nested functions in order of execution
-        Message.decode(req.body),
-        fold( // Takes handlers for two sides, calls whichever
-            errs => `Errors: ${
-                errs
-                .map(error => error.message)
-                .filter((x): x is string => x !== undefined)
-                .map(message => ` - ${message}\n`)
-            }`,
-            m => {
-                msgs.push(m)
-                return 'Received'
-            }
-        )
-    )
-})
+app.register(require('./routes'), {prefix: '/'});
 
-// Reply using callback
-app.get('/messages', (req, res) => {
-    res.send(msgs)
+app.listen(3000, (err, address) => {
+    if (err) {
+        app.log.error(err)
+        process.exit(1)
+    }
 })
-
-app.listen(80, '0.0.0.0')
---
+++ api/src/routes.ts
@@ -0,0 +1,38 @@
+import fastify from "fastify";
+
+import { Message } from "./common/message";
+import { pipe } from "fp-ts/lib/function";
+import { fold } from "fp-ts/lib/Either";
+
+// Ad-hoc database
+const msgs = new Array<Message>()
+const router = fastify({
+    logger: true
+})
+module.exports = (router: any, opts: any, done: any) => {
+
+    // Reply using return value
+    router.post('/send', async (req: any, res: any) => {
+        return pipe( // Allows to write nested functions in order of execution
+            Message.decode(req.body),
+            fold( // Takes handlers for two sides, calls whichever
+                errs => `Errors: ${errs
+                    .map(error => error.message)
+                    .filter((x): x is string => x !== undefined)
+                    .map(message => ` - ${message}\n`)
+                    }`,
+                m => {
+                    msgs.push(m)
+                    return 'Received'
+                }
+            )
+        )
+    })
+
+
+    // Reply using callback
+    router.get('/messages', (req: any, res: any) => {
+        res.send(msgs)
+    })
+
+}
