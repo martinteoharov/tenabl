634916c2c759ee602e30548e33bcd8bc6603b88b
Lawrence Bethlenfalvy <lbfalvy@protonmail.com>
2022-03-05 18:39:15 +0100

    Magic fix

--- api/ormconfig.js
+++ api/ormconfig.js
@@ -4,11 +4,11 @@ const subpath = path => isProduction ? `bin/api/src/db/${path}.js`
 
 module.exports = {
     type: 'postgres',
-    host: process.env.DB_HOST | 'localhost',
-    port: process.env.DB_PORT | 5432,
-    username: process.env.DB_USER | 'postgres',
-    password: process.env.DB_PASSWD | 'postgres',
-    database: process.env.DB_DATABASE | 'com2027_dev',
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5432,
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWD || 'postgres',
+    database: process.env.DB_DATABASE || 'com2027_dev',
     synchronize: !isProduction,
     logging: true,
     entities: [subpath('entities/*')],
