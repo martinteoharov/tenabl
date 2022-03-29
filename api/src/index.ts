51bf16c19d306dc0391315e040179b0eceade720
martinteoharov <martin.teoharov15@gmail.com>
2022-03-29 09:50:24 +0100

    setup jest

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-import fastify from "fastify";
+import fastify, { FastifyPluginAsync } from "fastify";
 import { connectToDB } from "./db";
 
-const App = (async () => {
+export const App = (async () => {
     // Note: Ensure connection to DB is established, before importing any routes
     await connectToDB();
 
@@ -12,7 +12,5 @@ const App = (async () => {
     })
         .register(require('./routes/auth'), { prefix: '/auth/' });
 
-    app.listen(80, '0.0.0.0')
+    app.listen(80, '0.0.0.0');
 })();
-
-export { App };
