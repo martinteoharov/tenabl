d7c7818bd9285a7684c7dcfc59ed2234df3794fc
martinteoharov <martin.teoharov15@gmail.com>
2022-03-29 21:45:37 +0100

    refactor tests

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,16 +1,16 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-import fastify, { FastifyPluginAsync } from "fastify";
+import fastify, { FastifyInstance, FastifyPluginAsync } from "fastify";
 import { connectToDB } from "./db";
 
-export const App = (async () => {
+export const build = async (): Promise<FastifyInstance> => {
     // Note: Ensure connection to DB is established, before importing any routes
     await connectToDB();
 
-    const app = fastify({
-        logger: true
-    })
-        .register(require('./routes/auth'), { prefix: '/auth/' });
+    const app = fastify({ logger: true });
+    app.register(require('./routes/auth'), { prefix: '/auth/' });
 
     app.listen(80, '0.0.0.0');
-})();
+
+    return app;
+};
