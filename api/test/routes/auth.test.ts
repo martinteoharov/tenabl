c397f027614ec51e1941069ec49922b8802dc24f
r1ghtwr0ng <rightwrong@protonmail.com>
2022-04-06 16:39:42 +0000

    Added more API tests

--- api/test/routes/auth.test.ts
+++ api/test/routes/auth.test.ts
@@ -1,4 +1,4 @@
-const { build } = require('../../src/index');
+import { build } from '../../src/index';
 
 describe('root tests', () => {
   let app:any;
@@ -10,38 +10,41 @@ describe('root tests', () => {
   test('user validation successful', async () => {
     const res = await app.inject({
       method: 'POST',
-      url: '/api/register',
-      payload: { first_name: "kur", last_name: "pedal", username: "kurpedal", email: "kurkopedal@gmail.com", password: "password", accepted_terms: true }
+      url: '/auth/register',
+      payload: { first_name: "Kur", last_name: "Pedal", username: "kurpedal", email: "kurkopedal@gmail.com", password: "P@ssword1", accepted_terms: true }
     })
 
     expect(res.statusCode).toEqual(200)
   })
 
-  // test('user validation unsuccessful: bad name', async () => {
-  //   const res = await app.inject({
-  //     method: 'GET',
-  //     url: '/'
-  //   })
+  test('user validation unsuccessful: bad name', async () => {
+    const res = await app.inject({
+      method: 'POST',
+      url: '/auth/register',
+      payload: { last_name: "Pedal", username: "Kurpedal", email: "kurkopedal@gmail.com", password: "P@ssword1", accepted_terms: true }
+    })
 
-  //   expect(JSON.parse(res.payload)).toEqual({ root: true })
-  // })
+    expect(res.statusCode).toEqual(400)
+  })
 
-  // test('user validation unsuccessful: bad email', async () => {
-  //   const res = await app.inject({
-  //     method: 'GET',
-  //     url: '/'
-  //   })
+  test('user validation unsuccessful: bad email', async () => {
+    const res = await app.inject({
+      method: 'POST',
+      url: '/auth/register',
+      payload: { first_name: "Kur", last_name: "Pedal", username: "Kurpedal", email: "kurkopedal", password: "P@ssword1", accepted_terms: true }
+  })
 
-  //   expect(JSON.parse(res.payload)).toEqual({ root: true })
-  // })
+    expect(res.statusCode).toEqual(400)
+  })
 
-  // test('user validation unsuccessful: bad password', async () => {
-  //   const res = await app.inject({
-  //     method: 'GET',
-  //     url: '/'
-  //   })
+  test('user validation unsuccessful: bad password', async () => {
+    const res = await app.inject({
+      method: 'POST',
+      url: '/auth/register',
+      payload: { first_name: "Kur", last_name: "Pedal", username: "Kurpedal", email: "kurkopedal@gmail.com", password: "password", accepted_terms: true }
+  })
 
-  //   expect(JSON.parse(res.payload)).toEqual({ root: true })
-  // })
+    expect(res.statusCode).toEqual(400)
+  })
 
 })
