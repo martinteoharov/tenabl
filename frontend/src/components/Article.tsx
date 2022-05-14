de67118480da2c52bd8155584fb976737f60b59e
martinteoharov <martin.teoharov15@gmail.com>
2022-05-14 20:43:10 +0100

    refactor article component and include it in home page

--- frontend/src/components/Article.tsx
+++ frontend/src/components/Article.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import '../styles/article.css'
+import { useNavigate } from "react-router-dom";
 
-interface Props {
+export interface ArticleProps {
     authorName: string;
     title: string;
     score: string;
+    url: string;
 }
 
-const Article: React.FC<Props> = ({authorName, title, score}) => {
+const Article: React.FC<ArticleProps> = ({ authorName, title, score, url }) => {
+    const navigate = useNavigate();
+
     return (
-        <div className="wrapper">
-            <div className="article">
-                <p className="author">{authorName}</p>
-                <p className="title">{title}</p>
-                <p className="score">{score}</p>
-            </div>     
+        <div onClick={() => navigate(url)} className="article">
+            <p className="author">{authorName}</p>
+            <p className="title">{title}</p>
+            <p className="score">{score}</p>
         </div>
     )
 }
